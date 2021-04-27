9a7c6555c942763e3b952c4dcf9f1c0dab26aae5
choi-haeun <71759430+choi-haeun@users.noreply.github.com>
2021-04-27 23:42:00 +0900

    springMVC5 expeption study

---
+++ src/main/java/com/spring/board/exception/ExceptionAdvice.java
@@ -0,0 +1,30 @@
+package com.spring.board.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import lombok.extern.log4j.Log4j;
+
+@ControllerAdvice
+@Log4j
+public class ExceptionAdvice {
+	
+	@ExceptionHandler(Exception.class)
+	public String except(Exception e,Model model) {
+		log.error("Exception...." + e.getMessage());
+		model.addAttribute("exception",e);
+		log.error(model);
+		
+		return "error_page";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException e) {
+		return "custom_error404";
+	}
+}
